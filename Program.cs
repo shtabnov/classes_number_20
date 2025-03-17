c94ff8f66b82a0630b341f8a9366220f4c406523
shtabnov <90615010+shtabnov@users.noreply.github.com>
2025-03-17 12:19:16 +0500

    create class product

--- Program.cs
+++ Program.cs
@@ -6,5 +6,39 @@ using System.Threading.Tasks;
 
 namespace classes_number_20
 {
-  
+    class Product
+    {
+        int index;
+        string name;
+        string store;
+        decimal price;
+
+        private static int counter = 0;
+
+        public Product(string name, string storeName, decimal price)
+        {
+            index = ++counter;
+            this.name = name;
+            this.store = storeName;
+            this.price = price;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string StoreName
+        {
+            get { return store; }
+            set { store = value; }
+        }
+
+        public decimal Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+    }  
 }
